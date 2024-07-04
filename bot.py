a10b70fb663cbbf7e6ffb23af8871439f57f34e2
Hankiii890 <shenderuk4kir@gmail.com>
2024-07-04 22:39:24 +0300

    Last check

--- bot.py
+++ bot.py
@@ -19,9 +19,6 @@ client = discord.Client(intents=intents)
 # Функция парсинга участников чата discord
 async def parse_participants(projects_name, discord_link):
     print(f'Пошёл парсинг!')
-    # Покдлючаемся к серверу
-    # guild_id = int(discord_link.split('/')[-1])
-    # guild = client.get_guild(guild_id)
     invite = await client.fetch_invite(discord_link)
     guild_id = invite.guild.id
     guild = client.get_guild(guild_id)
@@ -31,9 +28,13 @@ async def parse_participants(projects_name, discord_link):
     async for member in guild.fetch_members():
         members.append(member)
 
-    # Вставляем данные в БД
+    # Очищаем таблицу result перед заполнением новыми данными
     connect = connect_to_db()
     cur = connect.cursor()
+    cur.execute("TRUNCATE TABLE result RESTART IDENTITY")
+    connect.commit()
+
+    # Добавляем данные
     for value in members:
         cur.execute("INSERT INTO result (projects_name, login, role) VALUES (%s, %s, %s)", (projects_name, value.name, value.top_role.name))
     connect.commit()
@@ -51,25 +52,18 @@ async def parse_projects():
     cur.close()
 
 
-@tasks.loop(seconds=300)  # Run every 5 minutes
-async def parse_projects_loop():
-    await parse_projects()
-
-
 @client.event
 async def on_ready():
     print("Бот успешно запущен и подключён к серверу Discord!")
-    parse_projects_loop.start()
 
 
 # Запуск парсинга
 @client.event
 async def on_message(message):
-    print(f"(Канал: {message.channel.name}, Автор: {message.author.name})")
     if message.content == '!start_parsing':
         await parse_projects()
 
+    client.loop.create_task(parse_projects())
+
+
 client.run(DISCORD_TOKEN)
-
-
-
