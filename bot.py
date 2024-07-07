41430fd4a14fc13c90b9886b527d8e69a00515c2
Hankiii890 <shenderuk4kir@gmail.com>
2024-07-08 01:21:29 +0300

    parsing after a command on the server

--- bot.py
+++ bot.py
@@ -21,17 +21,17 @@ intents.members = True
 # Создаем экземпляр бота с заданным префиксом команд и настройками интентов
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 # Функция для парсинга и обновления данных участников проекта
 async def parse_participants(projects_name, discord_link):
     """Функция парсинга участников проекта, обновления CSV и базы данных"""
-
     # Получаем приглашение и сервер Discord
     invite = await bot.fetch_invite(discord_link)
     guild_id = invite.guild.id
     guild = bot.get_guild(guild_id)
 
     # Получаем список участников проекта
-    members = await guild.chunk()  # chunk используется для получения всех участников
+    members = guild.members
 
     # Записываем информацию в базу данных
     connect = connect_to_db()
@@ -43,15 +43,15 @@ async def parse_participants(projects_name, discord_link):
     cur.close()
 
     # Обновляем CSV файл
-    with open('results.csv', 'a', newline='') as csvfile:
+    with open('results.csv', 'a', newline='') as csvfile:  # Открываем файл CSV для добавления данных
         writer = csv.writer(csvfile)
         for member in members:
             writer.writerow([projects_name, member.name, member.top_role.name])
 
+
 # Функция для парсинга данных каждого проекта
 async def parse_projects():
     """Функция для парсинга данных каждого проекта и создания CSV файла"""
-
     # Получаем соединение с базой данных и выполняем запрос для получения проектов
     connect = connect_to_db()
     cur = connect.cursor()
@@ -65,14 +65,12 @@ async def parse_projects():
         await parse_participants(value[0], value[1])
     cur.close()
 
+
 # Обработчик события запуска бота
 @bot.event
 async def on_ready():
     print("Бот успешно запущен и подключен к серверу Discord!")
 
-    # При запуске бота создаем пустой CSV файл, если он еще не существует
-    with open('results.csv', 'a', newline='') as csvfile:
-        pass
 
 # Команда для запуска парсинга проектов
 @bot.command()
@@ -80,13 +78,6 @@ async def start_parsing(ctx):
     """Команда для запуска парсинга проектов"""
     await parse_projects()
     await ctx.send("CSV файл с данными успешно сгенерирован и данные добавлены в базу данных!")
-
-# Обработчик события нового сообщения
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    await bot.process_commands(message)
 
 # Запускаем бота с помощью предоставленного токена
 bot.run(DISCORD_TOKEN)
